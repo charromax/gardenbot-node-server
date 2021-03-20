bcf20d2397ac1c32474dab858d7efe76fdfd00ff
charromax <manuelrg88@gmail.com>
2021-03-20 13:17:36 -0300

    index fix

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-schema {
-  query: Query
-  mutation: Mutation
-}
-type User {
-  id: String!
-  name: String
-}
-type Query {
-  user (id: String): User
-}
-type Mutation {
-  addUser (
-    # Name for the User item
-    name: String!): User!
-}
+const { ApolloServer, PubSub } = require("apollo-server");
+const mongoose = require("mongoose");
+
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+const { MONGODB } = require("./config.js");
+
+const pubsub = new PubSub();
+
+const PORT = process.env.PORT || 5000;
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+	context: ({ req }) => ({ req, pubsub }), //take request and response from express and pass it along
+});
+
+mongoose
+	.connect(process.env.MONGODB_URI || MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		console.log("MongoDB connected");
+		return server.listen({ port: PORT });
+	})
+	.then((res) => {
+		console.log(`Server running at ${res.url}`);
+	})
+	.catch(err => {
+		console.log(err);
+	});
