2e271616368b0ad58240ea5efc88f899993140d5
charromax <manuelrg88@gmail.com>
2021-03-20 13:52:45 -0300

    asdasdas

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	MONGODB:
+		'mongodb+srv://charr0max:Mg412115@cluster0.y45id.mongodb.net/gardenbot?retryWrites=true&w=majority',
+	SECRET_KEY: 'a secret key',
+
+	ERROR_LOGIN: 'Error logging in',
+	USER_NOT_FOUND: 'User not found',
+	WRONG_PWD: 'Password is wrong',
+
+	
+};
