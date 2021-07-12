516a742ad52e3de858d7f8f918ae0ddaea60447b
charromax <manuelrg88@gmail.com>
2021-07-11 23:53:53 -0300

    asdasda

--- models/Measure.js
+++ models/Measure.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const measureSchema = mongoose.Schema({
-	createdAt: { type: String, default: new Date().toUTCString() },
+	createdAt: { type: String, default: new Date().toISOString() },
 	deviceId: String,
 	airTemp: Number,
 	airHum: Number,
